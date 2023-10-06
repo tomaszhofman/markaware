50b9ede0efba4a6ac73deefe4463e71715256d8d
tomaszhofman <tomasz.k.hofman@gmail.com>
2023-10-06 16:18:32 +0200

    initial setup

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	semi: true,
+	singleQuote: false,
+	trailingComma: "all",
+	printWidth: 100,
+	useTabs: true,
+	tabWidth: 2,
+	plugins: ["prettier-plugin-tailwindcss"],
+	tailwindConfig: "./tailwind.config.ts",
+};
